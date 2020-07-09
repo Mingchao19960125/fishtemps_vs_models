38dee5622b980cae98377891c31af556d2b7b4ea
Mingchao <55396095+Mingchao19960125@users.noreply.github.com>
2020-07-09 10:17:08 -0400

    Add files via upload

---
+++ Combine_vessels_hour_data/combine_vessels_hour_data.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 15:47:29 2020
+This function get data from checked folder,then create files include models'predicted temperature.
+Input:
+   checked folder of check_csv.py , get_clim folder
+Output:
+   files named like Excalibur_hours.csv.
+   The columns of files : ['lat','lon','temp','DOPPIO_T','GOMOFS_T','FVCOM_T','CLIM_T']
+
+@author: Jim&Mingchao
+"""
+import conversions as cv
+import os
+import pandas as pd
+import zlconversions as zl
+from datetime import datetime,timedelta
+from pylab import mean, std
+import multiple_models as mm
+import numpy as np
+
+#Hardcodes
+#input_dir='/home/jmanning/leizhao/programe/raw_data_match/result/checked/'
+input_dir='E:\\programe\\raw_data_match\\result\\checked\\'
+#end_time=datetime.now()
+end_time=datetime.utcnow()
+start_time=end_time-timedelta(days=300)
+#start_time=end_time-timedelta(weeks=1)
+#Hours_save='/home/jmanning/Mingchao/result/Hours_data/'
+Hours_save='E:\\Mingchao\\result\\Hours_data\\'
+getclim_path='E:\\programe\\aqmain\\py\\clim\\'
+    
+#main
+allfile_lists=zl.list_all_files(input_dir)
+file_lists=[]#store the path of every vessel's files
+#hoursfile_lists=zl.list_all_files(Hours_save)
+#filter the raw files and store in file_lists
+for file in allfile_lists:
+   if file[len(file)-4:]=='.csv':
+     file_lists.append(file)
+try:
+    for file in file_lists: # loop raw files         
+        fpath,fname=os.path.split(file)  #get the file's path and name
+        time_str=fname.split('.')[0].split('_')[2]+' '+fname.split('.')[0].split('_')[3]
+    #GMT time to local time of file
+        time_gmt=datetime.strptime(time_str,"%Y%m%d %H%M%S")
+        if time_gmt<start_time or time_gmt>end_time:
+            continue
+    # now, read header and data of every file  
+        header_df=zl.nrows_len_to(file,2,name=['key','value']) #only header 
+        data_df=zl.skip_len_to(file,2) #only data
+        value_data_df=data_df.loc[(data_df['Depth(m)']>0.95*max(data_df['Depth(m)']))]  #filter the data
+        value_data_df=value_data_df.iloc[7:]   #delay several minutes to let temperature sensor record the real bottom temp
+        value_data_df=value_data_df.loc[(value_data_df['Temperature(C)']>mean(value_data_df['Temperature(C)'])-3*std(value_data_df['Temperature(C)'])) & \
+                            (value_data_df['Temperature(C)']<mean(value_data_df['Temperature(C)'])+3*std(value_data_df['Temperature(C)']))]  #Excluding gross error
+        value_data_df.index = range(len(value_data_df))  #reindex
+        value_data_df['Datet(Y/m/d)']=1 #create a new column for saving another time style of '%Y-%m-%d'
+        for i in range(len(value_data_df)):
+            value_data_df['Lat'][i],value_data_df['Lon'][i]=cv.dm2dd(value_data_df['Lat'][i],value_data_df['Lon'][i])
+            #value_data_df['Datet(Y/m/d)'][i]=datetime.strptime(value_data_df['Datet(GMT)'][i],'%Y-%m-%d %H:%M:%S')
+        Hours_df=pd.DataFrame(data=None,columns=['time','lat','lon','temp','DOPPIO_T',\
+                                                 'GOMOFS_T','FVCOM_T','CLIM_T'])
+        Hours_df['time']=value_data_df['Datet(GMT)']
+        Hours_df['lat']=value_data_df['Lat']
+        Hours_df['lon']=value_data_df['Lon']
+        Hours_df['temp']=value_data_df['Temperature(C)']
+        Hours_df['time'] = pd.to_datetime( Hours_df['time'])
+        Hours_df['DOPPIO_T']=0.00
+        Hours_df['GOMOFS_T']=0.00
+        Hours_df['FVCOM_T']=0.00
+        Hours_df['CLIM_T']=0.00
+        Hours_df.index = Hours_df['time'].values
+        resample_df=Hours_df.loc[:,['lat','lon','temp','DOPPIO_T',\
+                             'GOMOFS_T','FVCOM_T','CLIM_T']].resample('H',how=np.mean)
+        for time_index in resample_df.index:
+            try:
+                resample_df['DOPPIO_T'][time_index]=mm.get_doppio_fitting(latp=resample_df['lat'][time_index],lonp=resample_df['lon'][time_index],\
+                                                   depth='bottom',dtime=time_index,fortype='temperature')
+            except:
+                resample_df['DOPPIO_T'][time_index]=np.nan
+            try:
+                resample_df['GOMOFS_T'][time_index]=mm.get_gomofs_zl(dtime=time_index,latp=resample_df['lat'][time_index],lonp=resample_df['lon'][time_index],\
+                                                   depth='bottom',mindistance=20,autocheck=True,fortype='temperature')
+            except:
+                resample_df['GOMOFS_T'][time_index]=np.nan
+            try:
+                resample_df['FVCOM_T'][time_index]=mm.get_FVCOM_fitting(latp=resample_df['lat'][time_index],lonp=resample_df['lon'][time_index],\
+                                                   dtime=time_index,depth='bottom',mindistance=2,fortype='temperature')
+            except:
+                resample_df['FVCOM_T'][time_index]=np.nan
+            try:
+                resample_df['CLIM_T'][time_index]=mm.getclim(lat1=resample_df['lat'][time_index],lon1=resample_df['lon'][time_index],\
+                                                   path=getclim_path,dtime=time_index,var='Bottom_Temperature\\BT_')
+            except:
+                resample_df['CLIM_T'][time_index]=np.nan
+        if not os.path.exists(Hours_save+file.split('\\')[5]):
+            os.makedirs(Hours_save+file.split('\\')[5])
+        resample_df.to_csv(os.path.join(Hours_save+file.split('\\')[5]+'\\',file.split('\\')[5]+'#'+file.split('\\')[7]))
+        hoursfile_together_lists=zl.list_all_files(Hours_save+file.split('\\')[5]+'\\')
+        total_hours_list=[]
+        for k in range(len(hoursfile_together_lists)):
+            total_hours_list.append(pd.read_csv(hoursfile_together_lists[k],index_col=0))#contact values belong to one vessel
+        total_hours_df=pd.concat(total_hours_list)
+        total_hours_df.drop_duplicates(subset=['lat','lon','temp'],inplace=True)
+        total_hours_df=total_hours_df.sort_index()#sorting by index
+        total_hours_df.to_csv(os.path.join(Hours_save+file.split('\\')[5]+'\\',hoursfile_together_lists[0].split('#')[0].split('\\')[4]+'_hours.csv'))
+except:
+    print(file+'check if the files exists')
+         
+
+
+       
+                
---
+++ Combine_vessels_hour_data/raw_data_time_series_plot.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 09:34:52 2020
+
+This function get time series plotting of fixed vessel's raw data.
+
+Input:
+   telemetry_status.csv,such as Excalibur_hours.csv
+Output:
+   a figure named such as Excalibur_raw_data_time_series_plot.png 
+
+@author: Mingchao
+"""
+
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import pandas as pd
+import zlconversions as zl
+import matplotlib.dates as dates
+from datetime import datetime,timedelta
+import matplotlib.ticker as ticker
+
+######################### Hardcodes ################################
+Hours_save = 'E:\\Mingchao\\result\\Hours_data\\'
+telemetry_status_path = 'E:\\Mingchao\\parameter\\telemetry_status.csv'
+start_time = datetime(2019,8,20,22,30,00)#temp_df.index[0]
+end_time = start_time+timedelta(weeks=8)
+model_list = ['DOPPIO_T', 'GOMOFS_T', 'FVCOM_T']
+
+def get_Fixed_vessel(telemetry_status_path):
+    '''get Fixed vessel's name'''
+    telemetrystatus_df = read_telemetrystatus(telemetry_status_path)
+    Fixed_vessel_list = []
+    for index in range(len(telemetrystatus_df)):
+        if telemetrystatus_df['Fixed vs. Mobile'][index]=='Fixed':
+            Fixed_vessel_list.append(telemetrystatus_df['Boat'][index])
+    return Fixed_vessel_list
+
+def read_telemetrystatus(path_name):
+    """read the telementry_status, then return the useful data"""
+    data=pd.read_csv(path_name)
+    #find the data lines number in the file('telemetry_status.csv')
+    for i in range(len(data['vessel (use underscores)'])):
+        if data['vessel (use underscores)'].isnull()[i]:
+            data_line_number=i
+            break
+    #read the data about "telemetry_status.csv"
+    telemetrystatus_df=pd.read_csv(path_name,nrows=data_line_number)
+    as_list=telemetrystatus_df.columns.tolist()
+    idex=as_list.index('vessel (use underscores)')
+    as_list[idex]='Boat'
+    telemetrystatus_df.columns=as_list
+    for i in range(len(telemetrystatus_df)):
+        telemetrystatus_df['Boat'][i]=telemetrystatus_df['Boat'][i].replace("'","")
+        if not telemetrystatus_df['Lowell-SN'].isnull()[i]:
+            telemetrystatus_df['Lowell-SN'][i]=telemetrystatus_df['Lowell-SN'][i].replace('，',',')
+        if not telemetrystatus_df['logger_change'].isnull()[i]:
+            telemetrystatus_df['logger_change'][i]=telemetrystatus_df['logger_change'][i].replace('，',',')
+    return telemetrystatus_df
+
+def plot(name,Hours_save,temp_df,start_time,end_time,dpi=300):
+        #name = vessel_lists.split('\\')[5].split('_hours')[0]
+        for time_index in temp_df.index:
+            if not start_time<=temp_df['new_time'][time_index]<=end_time:
+                temp_df = temp_df.drop(time_index)
+        temp_df.dropna(subset=['lat','lon'],inplace=True)
+        if len(temp_df)!=0:#make sure the dataframe have data in timerange
+            MIN_T=min(min(temp_df['temp']), min(temp_df['DOPPIO_T']),\
+                      min(temp_df['FVCOM_T']))
+            MAX_T=max(max(temp_df['temp']), max(temp_df['DOPPIO_T']),\
+                      max(temp_df['FVCOM_T']))
+            diff_temp=MAX_T-MIN_T
+            if diff_temp==0:
+                textend_lim=0.1
+            else:
+                textend_lim=diff_temp/8.0
+            fig=plt.figure(figsize=[11.69,8.27])
+            size=min(fig.get_size_inches())        
+            fig.suptitle(name,fontsize=3*size, fontweight='bold')
+            ax1=fig.add_subplot()
+            ax1.set_title(str(temp_df.index[0].split(' ')[0])+' to '+str(temp_df.index[-1].split(' ')[0]), fontsize=2*size)
+            vessel_index_list=[]#for storing the index that has big difference between last index
+            for vessel_index in range(len(temp_df)-1):
+                if "{:.4f}".format(temp_df['lon'][vessel_index]) != "{:.4f}".format(temp_df['lon'][vessel_index+1]):
+                    vessel_index_list.append(vessel_index+1)
+            if len(vessel_index_list) == 0:#the fishing period only once
+                 ax1.plot(temp_df['new_time'][:], temp_df['temp'][:], color='b', linewidth=3, label='Observed')
+                 ax1.plot(temp_df['new_time'][:], temp_df['DOPPIO_T'][:], color='r', linewidth=3, label='DOPPIO')
+                 ax1.plot(temp_df['new_time'][:], temp_df['FVCOM_T'][:], color='y', linewidth=3, label='FVCOM')
+                 ax1.plot(temp_df['new_time'][:], temp_df['GOMOFS_T'][:], color='black', linewidth=3, label='GOMOFS')
+                 ax1.plot(temp_df['new_time'][:], temp_df['CLIM_T'][:], color='g', linewidth=3, label='Climatology')
+            elif len(vessel_index_list) == 1:#the fishing period only twice
+                 ax1.plot(temp_df['new_time'][:vessel_index_list[0]], temp_df['temp'][:vessel_index_list[0]], color='b', linewidth=3, label='Observed')
+                 ax1.plot(temp_df['new_time'][:vessel_index_list[0]], temp_df['DOPPIO_T'][:vessel_index_list[0]], color='r', linewidth=3, label='DOPPIO')
+                 ax1.plot(temp_df['new_time'][:vessel_index_list[0]], temp_df['FVCOM_T'][:vessel_index_list[0]], color='y', linewidth=3, label='FVCOM')
+                 ax1.plot(temp_df['new_time'][:vessel_index_list[0]], temp_df['GOMOFS_T'][:vessel_index_list[0]], color='black', linewidth=3, label='GOMOFS')
+                 ax1.plot(temp_df['new_time'][:vessel_index_list[0]], temp_df['CLIM_T'][:vessel_index_list[0]], color='g', linewidth=3, label='Climatology')
+                 ax1.plot(temp_df['new_time'][vessel_index_list[0]:], temp_df['temp'][vessel_index_list[0]:], color='b', linewidth=3)
+                 ax1.plot(temp_df['new_time'][vessel_index_list[0]:], temp_df['DOPPIO_T'][vessel_index_list[0]:], color='r', linewidth=3)
+                 ax1.plot(temp_df['new_time'][vessel_index_list[0]:], temp_df['FVCOM_T'][vessel_index_list[0]:], color='y', linewidth=3)
+                 ax1.plot(temp_df['new_time'][vessel_index_list[0]:], temp_df['GOMOFS_T'][vessel_index_list[0]:], color='black', linewidth=3)
+                 ax1.plot(temp_df['new_time'][vessel_index_list[0]:], temp_df['CLIM_T'][vessel_index_list[0]:], color='g', linewidth=3)
+            else:
+                for number in range(len(vessel_index_list)):
+                    if number == 0:#the first fishing period
+                        ax1.plot(temp_df['new_time'][:vessel_index_list[0]], temp_df['temp'][:vessel_index_list[0]], color='b', linewidth=3, label='Observed')
+                        ax1.plot(temp_df['new_time'][:vessel_index_list[0]], temp_df['DOPPIO_T'][:vessel_index_list[0]], color='r', linewidth=3, label='DOPPIO')
+                        ax1.plot(temp_df['new_time'][:vessel_index_list[0]], temp_df['FVCOM_T'][:vessel_index_list[0]], color='y', linewidth=3, label='FVCOM')
+                        ax1.plot(temp_df['new_time'][:vessel_index_list[0]], temp_df['GOMOFS_T'][:vessel_index_list[0]], color='black', linewidth=3, label='GOMOFS')
+                        ax1.plot(temp_df['new_time'][:vessel_index_list[0]], temp_df['CLIM_T'][:vessel_index_list[0]], color='g', linewidth=3, label='Climatology')
+                    elif number == len(vessel_index_list)-1:#the last fishing period
+                        ax1.plot(temp_df['new_time'][vessel_index_list[number]:], temp_df['temp'][vessel_index_list[number]:], color='b', linewidth=3)
+                        ax1.plot(temp_df['new_time'][vessel_index_list[number]:], temp_df['DOPPIO_T'][vessel_index_list[number]:], color='r', linewidth=3)
+                        ax1.plot(temp_df['new_time'][vessel_index_list[number]:], temp_df['FVCOM_T'][vessel_index_list[number]:], color='y', linewidth=3)
+                        ax1.plot(temp_df['new_time'][vessel_index_list[number]:], temp_df['GOMOFS_T'][vessel_index_list[number]:], color='black', linewidth=3)
+                        ax1.plot(temp_df['new_time'][vessel_index_list[number]:], temp_df['CLIM_T'][vessel_index_list[number]:], color='g', linewidth=3)
+                    else:
+                        ax1.plot(temp_df['new_time'][vessel_index_list[number]:vessel_index_list[number+1]], temp_df['temp'][vessel_index_list[number]:vessel_index_list[number+1]], color='b', linewidth=3)
+                        ax1.plot(temp_df['new_time'][vessel_index_list[number]:vessel_index_list[number+1]], temp_df['DOPPIO_T'][vessel_index_list[number]:vessel_index_list[number+1]], color='r', linewidth=3)
+                        ax1.plot(temp_df['new_time'][vessel_index_list[number]:vessel_index_list[number+1]], temp_df['FVCOM_T'][vessel_index_list[number]:vessel_index_list[number+1]], color='y', linewidth=3)
+                        ax1.plot(temp_df['new_time'][vessel_index_list[number]:vessel_index_list[number+1]], temp_df['GOMOFS_T'][vessel_index_list[number]:vessel_index_list[number+1]], color='black', linewidth=3)
+                        ax1.plot(temp_df['new_time'][vessel_index_list[number]:vessel_index_list[number+1]], temp_df['CLIM_T'][vessel_index_list[number]:vessel_index_list[number+1]], color='g', linewidth=3)
+            ax12=ax1.twinx()
+            ax12.set_ylabel('Fahrenheit',fontsize=2*size)
+            #conversing the Celius to Fahrenheit
+            ax12.set_ylim((MAX_T+textend_lim)*1.8+32,(MIN_T-textend_lim)*1.8+32)
+            ax12.invert_yaxis()
+            ax1.set_ylabel('Celsius',fontsize=2*size)
+            ax1.set_ylim()#MIN_T-textend_lim,MAX_T+textend_lim
+            ax1.legend(prop={'size':2* size})
+            ax1.tick_params(labelsize=1.5*size)
+            plt.gca().xaxis.set_major_formatter(dates.DateFormatter('%Y-%m-%d'))
+            #fig.autofmt_xdate() 
+            plt.gcf().autofmt_xdate()
+        else:
+            print(name+' does not have valuable data in this time range!')
+#        if not os.path.exists(os.path.join(Hours_save+vessel_lists[i].split('/')[6].split('_hours')[0]+'/')):
+#            os.makedirs(Hours_save+vessel_lists[i].split('/')[6].split('_hours')[0]+'/')
+#        plt.savefig(os.path.join(Hours_save+vessel_lists[i].split('/')[6].split('_hours')[0]+'/')+vessel_lists[i].split('/')[6].split('_hours')[0]+'_hours.ps',dpi=dpi,orientation='landscape')
+        plt.savefig(Hours_save+name+'\\'+name+'_raw_data_time_series_plot.png',dpi=dpi,orientation='portait')
+        plt.show()
+        
+####################### main ######################
+hours_lists=zl.list_all_files(Hours_save)
+vessel_lists=[]#store the path of every vessel's file
+Fixed_vessel_list = get_Fixed_vessel(telemetry_status_path)
+#Loop every vessel's file and Plot
+for file in hours_lists:
+   if file[len(file)-9:]=='hours.csv':
+     vessel_lists.append(file)                
+for i in range(len(vessel_lists)):
+    name = vessel_lists[i].split('\\')[5].split('_hours')[0]
+    if name in Fixed_vessel_list:
+        vessel_df=pd.read_csv(vessel_lists[i],index_col=0)
+        vessel_df['new_time'] = vessel_df.index
+        vessel_df['new_time'] = pd.to_datetime(vessel_df['new_time'])#change the time style to datetime
+        vessel_df.drop_duplicates(subset=['new_time'],inplace=True)
+        plot(name=name,Hours_save=Hours_save,temp_df=vessel_df,start_time=start_time,end_time=end_time,dpi=300)
+    else:
+        continue

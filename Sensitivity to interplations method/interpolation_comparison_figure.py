7bc3937d0d110389f20930f2b2bb9dcfc3013d7a
Mingchao <55396095+Mingchao19960125@users.noreply.github.com>
2020-07-29 21:02:25 -0400

    Add files via upload

--- Sensitivity to interplations method/interpolation_comparison_figure.py
+++ Sensitivity to interplations method/interpolation_comparison_figure.py
@@ -5,10 +5,41 @@ Created on Mon Jul 27 17:47:14 2020
 @author: Mingchao
 """
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import os
 from matplotlib import gridspec
+import math
 
+def multipl(a,b):
+    '''
+    Input two sequences
+    Output the sum of the products of the two sequences
+    '''
+    sumofab = 0.0
+    for i in range(len(a)):
+        temp = a[i]*b[i]
+        sumofab += temp
+    return sumofab
+
+def corrcoef(x,y):
+    '''
+    Input two sequences
+    Output Correlation coefficient of two series
+    '''
+    n = len(x)
+    #sum
+    sum1 = sum(x)
+    sum2 = sum(y)
+    #Find the sum of products
+    sumofxy = multipl(x,y)
+    #Find the sum of squares
+    sumofx2 = sum([pow(i,2) for i in x])
+    sumofy2 = sum([pow(j,2) for j in y])
+    num = sumofxy-(float(sum1)*float(sum2)/n)
+    #Calculate Pearson's correlation coefficient
+    den = math.sqrt((sumofx2-float(sum1**2)/n)*(sumofy2-float(sum2**2)/n))
+    return num/den
 ###################### Hardcodes #########################
 path = 'E:\\Mingchao\\paper\\interpo_comparison_data.csv'
 path_save = 'E:\\Mingchao\\paper'
@@ -53,5 +84,10 @@ for i in range(4):
     ax_list[i].set_ylim(MIN_T-textend_lim,MAX_T+textend_lim)
     ax_list[i].set_title(seasons[i])
     ax_list[i].legend()
-fig.suptitle('Seasonal comparison of Interpolation methods')
+    print(seasons[i]+':')
+    print('observation mean:%f'%np.mean(observation_list),'fitting mean:%f'%np.mean(fitting_list),'no_fitting mean:%f'%np.mean(nofitting_list))
+    print('observation std:%f'%np.std(observation_list,ddof=1),'fitting std:%f'%np.std(fitting_list,ddof=1),'no_fitting std:%f'%np.std(nofitting_list,ddof=1))
+    print('observation var:%f'%np.var(observation_list),'fitting var:%f'%np.var(fitting_list),'no_fitting var:%f'%np.var(nofitting_list))
+    print('fitting mean:%f'%corrcoef(fitting_list,observation_list),'no_fitting mean:%f'%corrcoef(nofitting_list,observation_list))
+fig.suptitle('Seasonal comparison of Interpolation methods',fontsize=20)
 plt.savefig(os.path.join(path_save,'interpolation_comparison_figure.png'))

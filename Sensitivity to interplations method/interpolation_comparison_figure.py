12d06014a1a50443657cd602869e5e6d0c56ca3a
Mingchao <55396095+Mingchao19960125@users.noreply.github.com>
2020-08-07 11:47:43 -0400

    Add files via upload

--- Sensitivity to interplations method/interpolation_comparison_figure.py
+++ Sensitivity to interplations method/interpolation_comparison_figure.py
@@ -2,14 +2,17 @@
 """
 Created on Mon Jul 27 17:47:14 2020
 
+Plot time series and scatter of Interpolation methods
+
 @author: Mingchao
 """
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.dates as dates
-import sklearn
+import os
+from sklearn.metrics import mean_squared_error
 import math
+import datetime
 from matplotlib import gridspec
 from scipy import stats
 
@@ -18,7 +21,7 @@ def rmse(actual, predicted):
     get Root Mean Square Error
     actual is observation data,predicted is model's data
     '''
-    mse = sklearn.metrics.mean_squared_error(actual, predicted)
+    mse = mean_squared_error(actual, predicted)
     rmse = math.sqrt(mse)
     return float(rmse)
 
@@ -52,18 +55,36 @@ def corrcoef(x,y):
     den = math.sqrt((sumofx2-float(sum1**2)/n)*(sumofy2-float(sum2**2)/n))
     return num/den
 ###################### Hardcodes #########################
-path = 'E:\\Mingchao\\paper\\interpo_comparison_data.csv'
+path = 'E:\\Mingchao\\paper\\interpo_comparison_data_final.csv'
 path_save = 'E:\\Mingchao\\paper'
 seasons = ['WINTER', 'SPRING', 'SUMMER', 'FALL']
+start_time = datetime.datetime(2018,8,1,00,00,00) # 8 9
+end_time = datetime.datetime(2018,9,1,00,00,00)
 ###################### Main ##############################
 comparison_df = pd.read_csv(path,index_col=0)
 comparison_df = comparison_df.dropna()
+comparison_df_scatter = comparison_df
 comparison_df.index = pd.to_datetime(comparison_df['time'])
+for i in comparison_df.index:
+    if not start_time<i<end_time:
+        comparison_df['time'][i]=np.nan
+comparison_df = comparison_df.dropna(subset=['time'])
 comparison_df['month'] = comparison_df.index.month
 season = ((comparison_df.month % 12 + 3) // 3).map({1:'WINTER', 2: 'SPRING', 3:'SUMMER', 4:'FALL'})
 comparison_df['season'] = season.values 
 fig = plt.figure()#figsize=(11.69,8.27)
-ax = fig.add_subplot()
+gsc = gridspec.GridSpec(2,2,wspace=0.04, hspace=0.04,width_ratios=[1, 1])
+ax1 = fig.add_subplot(gsc[0,0])
+ax2 = fig.add_subplot(gsc[0,1])
+ax3 = fig.add_subplot(gsc[1,0])
+ax4 = fig.add_subplot(gsc[1,1])
+ax1.axes.get_xaxis().set_visible(False)
+ax2.axes.get_xaxis().set_visible(False)
+ax2.axes.get_yaxis().set_visible(False)
+ax4.axes.get_yaxis().set_visible(False)
+observation_list = list(comparison_df_scatter['observation_T'])#.loc[comparison_df['season']==seasons[i]])
+fitting_list = list(comparison_df_scatter['fitting'])#.loc[comparison_df['season']==seasons[i]])
+nofitting_list = list(comparison_df_scatter['no_fitting'])#.loc[comparison_df['season']==seasons[i]])
 size = min(fig.get_size_inches())
 MAX_T=max(comparison_df['observation_T'].values)
 MIN_T=min(comparison_df['observation_T'].values)
@@ -72,42 +93,32 @@ if diff_temp==0:
     textend_lim=0.1
 else:
     textend_lim=diff_temp/8.0
-ax.plot(comparison_df.index,comparison_df['observation_T'].values,color='r',label='Observation')
-ax.plot(comparison_df.index,comparison_df['fitting'].values,color='blue',label='Bilinear Interpolation')
-ax.plot(comparison_df.index,comparison_df['no_fitting'].values,color='green',alpha=0.8,label='Nearest Interpolation')
-ax.set_ylabel('Celsius',fontsize=3.5*size)
-ax.set_ylim(MIN_T-textend_lim,MAX_T+textend_lim)
-ax.set_title('2018.5 to 2019.5')
-ax.legend(prop={'size':2* size})
-ax.tick_params(labelsize=2.5*size)
-plt.gca().xaxis.set_major_formatter(dates.DateFormatter('%Y-%m-%d'))
-plt.gcf().autofmt_xdate()
-ax.legend()
-#gsc = gridspec.GridSpec(2,2,wspace=0.02, hspace=0.09,width_ratios=[1, 1])
-#ax1 = fig.add_subplot(gsc[0,0])
-#ax1.axes.get_xaxis().set_visible(False)
-#ax2 = fig.add_subplot(gsc[0,1])
-#ax2.axes.get_xaxis().set_visible(False)
-#ax2.axes.get_yaxis().set_visible(False)
-#ax3 = fig.add_subplot(gsc[1,0])
-#ax3.axes.get_xaxis().set_visible(False)
-#ax4 = fig.add_subplot(gsc[1,1])
-#ax4.axes.get_yaxis().set_visible(False)
-#ax4.axes.get_xaxis().set_visible(False)
-#ax_list = [ax1,ax2,ax3,ax4]
-#for i in range(4):
-#    time_list = list(comparison_df.index[comparison_df['season']==seasons[i]])
-observation_list = list(comparison_df['observation_T'])#.loc[comparison_df['season']==seasons[i]])
-fitting_list = list(comparison_df['fitting'])#.loc[comparison_df['season']==seasons[i]])
-nofitting_list = list(comparison_df['no_fitting'])#.loc[comparison_df['season']==seasons[i]])
-#    ax_list[i].plot(time_list,observation_list,color='r',label='Observation')
-#    ax_list[i].plot(time_list,fitting_list,linewidth=2,color='blue',label='Bilinear Interpolation')
-#    ax_list[i].plot(time_list,nofitting_list,linewidth=2,color='green',label='Nearest Interpolation')
-#    ax_list[i].set_ylabel('Celsius',fontsize=2*size)
-#    ax_list[i].set_ylim(MIN_T-textend_lim,MAX_T+textend_lim)
-#    ax_list[i].set_title(seasons[i])
-#    ax_list[i].legend()
-#    print(seasons[i]+':')
+ax1.plot(comparison_df.index,comparison_df['observation_T'].values,color='r',label='Observation')
+ax1.plot(comparison_df.index,comparison_df['fitting'].values,color='blue',alpha=0.8,label='Bilinear Interpolation')
+ax1.set_ylabel('Celsius',fontsize=3.5*size)
+ax1.set_ylim(MIN_T-textend_lim,MAX_T+textend_lim)
+ax1.set_title('Bilinear Interpolation')
+ax1.legend(prop={'size':2* size})
+ax1.tick_params(labelsize=2.5*size)
+#plt.gca().xaxis.set_major_formatter(dates.DateFormatter('%Y-%m-%d'))
+#plt.gcf().autofmt_xdate()
+ax2.plot(comparison_df.index,comparison_df['observation_T'].values,color='r',label='Observation')
+ax2.plot(comparison_df.index,comparison_df['no_fitting'].values,color='green',alpha=0.8,label='Nearest Interpolation')
+ax2.set_title('Nearest Interpolation')
+ax2.legend(prop={'size':2* size})
+ax2.tick_params(labelsize=2.5*size)
+
+############# scatter #################
+a = ax3.hist2d(x=comparison_df_scatter['fitting'].values, y=comparison_df_scatter['observation_T'].values, bins=40, alpha=0.9, cmin=1)
+ax3.set_xlim(0,25)
+ax3.set_ylim(0,25)
+ax3.plot([0,25],[0,25],color='red',linewidth=1,alpha=0.8)
+ax3.set_ylabel('Observation',fontsize=3.5*size)
+b = ax4.hist2d(x=comparison_df_scatter['no_fitting'].values, y=comparison_df_scatter['observation_T'].values, bins=40, alpha=0.9, cmin=1)
+ax4.set_xlim(0,25)
+ax4.set_ylim(0,25)
+ax4.plot([0,25],[0,25],color='red',linewidth=1,alpha=0.8)
+plt.colorbar(a[3], ax=[ax3, ax4])
 print('observation mean:%.2f'%np.mean(observation_list),'fitting mean:%.2f'%np.mean(fitting_list),'no_fitting mean:%.2f'%np.mean(nofitting_list))
 print('observation std:%.2f'%np.std(observation_list),'fitting std:%.2f'%np.std(fitting_list),'no_fitting std:%.2f'%np.std(nofitting_list))
 print('observation var:%.2f'%np.var(observation_list),'fitting var:%.2f'%np.var(fitting_list),'no_fitting var:%.2f'%np.var(nofitting_list))
@@ -115,4 +126,4 @@ print('fitting bias:%.2f'%(np.mean(observation_list)-np.mean(fitting_list)),'no_
 print('fitting Correlation coefficient:%.2f'%corrcoef(fitting_list,observation_list),'no_fitting Correlation coefficient:%.2f'%corrcoef(nofitting_list,observation_list))
 print('fitting RMSE:%.2f'%rmse(observation_list,fitting_list),'no_fitting RMSE:%.2f'%rmse(observation_list,nofitting_list))
 fig.suptitle('Comparison of Interpolation methods',fontsize=16)
-#plt.savefig(os.path.join(path_save,'interpolation_comparison_figure.png'),dpi=300)
+plt.savefig(os.path.join(path_save,'interpolation_comparison_figure.png'),dpi=300)
